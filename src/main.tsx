a200af6c823235d8f3e447d03bf84cb6b3cbd7d4
Aditya Patra <adityapatra60@gmail.com>
2025-11-01 12:40:07 +0530

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { store } from './app/store'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import App from './routes/App'
-import RecipesPage from './routes/RecipesPage'
-import CreatePage from './routes/CreatePage'
-import CookPage from './routes/CookPage'
-import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import { createHashRouter, RouterProvider } from 'react-router-dom'; 
+import App from './routes/App';
+import RecipesPage from './routes/RecipesPage';
+import CreatePage from './routes/CreatePage';
+import CookPage from './routes/CookPage';
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
     element: <App />,
     children: [
-      { path: '/recipes', element: <RecipesPage /> },
-      { path: '/create', element: <CreatePage /> },
-      { path: '/cook/:id', element: <CookPage /> },
-      { index: true, element: <RecipesPage /> }, // default to /recipes
-      { path: '*', element: <div style={{padding:24}}>Not found</div> }, // catch-all
+      { index: true, element: <RecipesPage /> },          
+      { path: 'recipes', element: <RecipesPage /> },     
+      { path: 'create', element: <CreatePage /> },        
+      { path: 'cook/:id', element: <CookPage /> },        
+      { path: '*', element: <div style={{ padding: 24 }}>Not found</div> },
     ],
   },
-])
+]);
 
-const theme = createTheme({ palette: { mode: 'light' } })
+const theme = createTheme({ palette: { mode: 'light' } });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -34,4 +34,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       </ThemeProvider>
     </Provider>
   </React.StrictMode>
-)
+);
